ea79efc6aff4c3e08180306ad6668e76185aee49
AlexandraHessen <xl_energy@mail.ru>
2021-11-08 19:24:33 +0300

    props_propTypes

---
+++ B_props_propTypesVB1/VotesBlock120_props/VotesBlock.js
@@ -0,0 +1,46 @@
+﻿// Описание нового компонента с именем ИмяКомпонента:
+
+// var ИмяКомпонента = React.createClass({
+//   displayName: "отображаемое имя компонента",
+//   render: function () {
+//     return React.createElement(...);
+//   },
+// })
+
+var VotesBlock = React.createClass({
+// react все делит на стандартные теги и наши компоненты
+// все стандартные теги с маленькой буквы наши компоненты всегда с Большой
+  displayName: 'VotesBlock',
+
+  // если props не передан оно сработает
+  getDefaultProps: function() {
+    return { question: "Вопрос ни о чём" }
+  },
+  // эта запятая ОБЯЗАТЕЛЬНА!
+
+  render: function() {
+
+    var answersCode=[];
+    for ( var a=0; a<this.props.answers.length; a++ ) {
+      var answer=this.props.answers[a];
+      var answerCode=        
+        React.DOM.div({key:answer.code,className:'Answer'},
+        //key нельзя использовать индекс массива i
+          React.DOM.span({className:'Count'},answer.count),
+          React.DOM.span({className:'Text'},answer.text),
+        );
+      answersCode.push(answerCode);
+    }
+
+    // render возвращает всегда только 1 тег, если нужно больше обаращиваем все в div
+    return React.DOM.div( {className:'VotesBlock'}, 
+    //первый всегда атрибуты
+    // все остальные параменты это содержимое ( второй и тд)
+      React.DOM.div( {className:'Question'}, this.props.question ),
+      // все что передано мне в качестве атрибута в мой компонент, я внутри компонента вижу под именем this.props
+      React.DOM.div( {className:'Answers'}, answersCode ),
+    );
+  },
+  // эта запятая ОБЯЗАТЕЛЬНА!
+
+});
---
+++ B_props_propTypesVB1/VotesBlock130_newprops/VotesBlock.js
@@ -0,0 +1,41 @@
+﻿var VotesBlock = React.createClass({
+
+  displayName: 'VotesBlock',
+
+  render: function() {
+        var answersCode=this.props.answers.map( v =>
+            React.DOM.div({key:v.code,className:'Answer'},
+              React.DOM.span({className:'Count'},v.count),
+              React.DOM.span({className:'Text'},v.text),
+            )
+          ); 
+
+        return React.DOM.div( {className:'VotesBlock'}, 
+          React.DOM.div( {className:'Question'}, this.props.question ),
+          React.DOM.div( {className:'Answers'}, answersCode ),
+        );
+      },
+    
+});
+// map 
+//выполняет указанную функцию для каждого элемента массива последовательно; 
+//из значений, которые вернёт функция, составляется новый массив
+// Описание нового компонента с именем ИмяКомпонента:
+
+// var ИмяКомпонента = React.createClass({
+//   displayName: "отображаемое имя компонента",
+//   render: function () {
+//     return React.createElement(...);
+//   },
+// })
+
+// react все делит на стандартные теги и наши компоненты
+// все стандартные теги с маленькой буквы наши компоненты всегда с Большой
+
+// React.DOM.тэг(хэш с атрибутами, содержимое)
+// создать HTML-тег; в хэше с атрибутами — атрибуты тега, 
+// только следует использовать не нативные имена HTML-атрибутов, а имена соответствующих им DOM-свойств, если они различаются (htmlFor, className)
+
+// render возвращает всегда только 1 тег, если нужно больше обаращиваем все в div
+ //первый всегда атрибуты
+// все остальные параменты это содержимое ( второй и тд)
