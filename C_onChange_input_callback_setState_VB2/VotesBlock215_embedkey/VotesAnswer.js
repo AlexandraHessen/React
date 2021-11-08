93e7bb6a8a85b1a122cebe2e2ff8d1e3add4c878
AlexandraHessen <xl_energy@mail.ru>
2021-11-08 19:27:16 +0300

    onChange_input_callback_setState

---
+++ C_onChange_input_callback_setState_VB2/VotesBlock215_embedkey/VotesAnswer.js
@@ -0,0 +1,25 @@
+﻿var VotesAnswer = React.createClass({
+
+  displayName: 'VotesAnswer',
+
+  propTypes: {
+    count: React.PropTypes.number.isRequired,
+    text: React.PropTypes.string.isRequired,
+  },
+
+  render: function() {
+
+    return React.DOM.div( {className:'VotesBlockAnswer'},
+      React.DOM.span({className:'Count'},this.props.count),
+      React.DOM.span({className:'Text'},this.props.text),
+    );
+  },
+
+});
+
+// var answersCode=this.props.answers.map( v =>
+//   React.DOM.div({key:v.code,className:'Answer'},
+//     React.DOM.span({className:'Count'},v.count),
+//     React.DOM.span({className:'Text'},v.text),
+//   )
+// );
---
+++ C_onChange_input_callback_setState_VB2/VotesBlock220_transprops/VotesAnswer.js
@@ -0,0 +1,33 @@
+﻿var VotesAnswer = React.createClass({
+
+  displayName: 'VotesAnswer',
+
+  propTypes: {
+    code: React.PropTypes.number.isRequired,
+    count: React.PropTypes.number.isRequired,
+    text: React.PropTypes.string.isRequired,
+    workMode: React.PropTypes.number.isRequired, // 1- прием голоса, 2- отображение результата
+  },
+
+  render: function() {
+
+    if ( this.props.workMode==1 ) {
+      return React.DOM.div(null,
+        React.DOM.label({className:'VotesBlockAnswer'},
+          React.DOM.input({type:'radio',value:this.props.code,name:'voteanswer'}),
+          React.DOM.span(null,this.props.text)
+          // положить input и span в один label позволяет пользователю выбирать вариант щелчком по тексту
+        ),
+      );
+    }
+    else {
+      return React.DOM.div( {className:'VotesBlockAnswer'},
+        React.DOM.span({className:'Count'},this.props.count),
+        React.DOM.span({className:'Text'},this.props.text),
+      );
+    }
+
+
+  },
+
+});
