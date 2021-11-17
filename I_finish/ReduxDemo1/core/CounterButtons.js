54d38398a9399605cd92379ac532fda669637122
AlexandraHessen <xl_energy@mail.ru>
2021-11-17 23:25:23 +0300

    CounterButtons

--- I_finish/ReduxDemo1/core/CounterButtons.js
+++ I_finish/ReduxDemo1/core/CounterButtons.js
@@ -26,6 +26,8 @@ class intCounterButtons extends React.PureComponent {
 
 }
 
+//---------------- 1 Вариант ----------------//
+// ПРОЧИТАТЬ COMMENT 2 ВАРИАНТА
 const mapStateToProps = function (state) {
   // этому компоненту ничего не нужно из хранилища Redux
   return { }; 
@@ -33,6 +35,40 @@ const mapStateToProps = function (state) {
 
 // но этому компоненту нужен сам this.props.dispatch, и чтобы
 // он появился, следует присоединить (connect) компонент к хранилищу Redux
+
+// заворачиваем в HOC
+// connect  import {connect} from 'react-redux'; 
+// позволяет React подписаться на Redux 
+// connect в качестве аргумента дожен получить хэш, который говорит 
+// что из Redux под какими props должно прилететь
 const CounterButtons = connect(mapStateToProps)(intCounterButtons);
+
+
+//---------------- 2 Вариант ----------------//
+// можно вообще не указывать mapStateToProps
+// если мы не передаем первый аргумент const CounterButtons = connect()(intCounterButtons);
+// то connect передает сам dispatch
+
+//т.е.
+
+//* если передали аргумент mapStateToProps
+// тем самым сказали предоставь мне в качестве props функции, т.е. callback которые мне нужно дернуть,
+// чтобы увеливать счетчик на 1 или 0
+
+//* если НЕ передали аргумент mapStateToProps
+// т.е. не говорю какие действия четко вызывать, не готовь для меня callback,
+// дай мне сам dispath, я его сам вызову и сам ему передам action 
+
+
+// но этому компоненту нужен сам this.props.dispatch, и чтобы
+// он появился, следует присоединить (connect) компонент к хранилищу Redux
+
+
+// заворачиваем в HOC
+// connect  import {connect} from 'react-redux'; 
+// позволяет React подписаться на Redux 
+// connect в качестве аргумента дожен получить хэш, который говорит 
+// что из Redux под какими props должно прилететь
+const CounterButtons = connect()(intCounterButtons);
 
 export default CounterButtons;
