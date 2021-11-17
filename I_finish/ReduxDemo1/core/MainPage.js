24f703d1f085293c10253c82aaef0c0ef287e67e
AlexandraHessen <xl_energy@mail.ru>
2021-11-17 23:54:57 +0300

    Provider store

--- I_finish/ReduxDemo1/core/MainPage.js
+++ I_finish/ReduxDemo1/core/MainPage.js
@@ -13,6 +13,13 @@ let combinedReducer=combineReducers({
     // counter - название раздела Redux и им будет управлять ruducer - counterReducer
     // + другие редьюсеры
 });
+
+/*
+1. мы написали 1 reducer (counterReducer)
+2. из него собрали комбинированный reducer (combinedReducer), т.е. reducer управляющий всем Redux сразу
+3. сказали Redux создай Store (хранилище данных), которым управляет combinedReducer
+*/
+
 let store=createStore(combinedReducer);
 // createStore - СОЗДАТЬ REDUX
 class MainPage extends React.PureComponent {
@@ -21,6 +28,11 @@ class MainPage extends React.PureComponent {
 
     return (
       <Provider store={store}>
+      {/* Provider - import { Provider } from 'react-redux'; 
+      все свое приложения мы заварачиваем в tag Provider
+      и в нем сказать store={store} - (хранилище) т.е. раздел Redux 
+      т.е. это хранилище должно быть доступно всему коду, который внутри Provider
+      */}
           <div>
               <h1>демо работы Redux</h1>
               <CounterButtons />
