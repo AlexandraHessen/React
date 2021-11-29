409580ada4b2819828e7da23a79d0303e8ff1513
AlexandraHessen <xl_energy@mail.ru>
2021-11-29 18:06:09 +0300

    counterReducer

--- I_finish/ReduxDemo1/core/counterReducer.js
+++ I_finish/ReduxDemo1/core/counterReducer.js
@@ -2,19 +2,22 @@
   cnt: 0,
 };
 
-// в редьюсере state - это не весь state Redux, а только тот раздел state,
-// за который отвечает данный редьюсер
+//  -----------------------  1  ----------------------- //
+// в Reducer state - это не весь state Redux, а только тот раздел state,
+// за который отвечает данный Reducer
 
 function counterReducer(state=initState,action) {
-// state- текущее состояние Redux 
-// action -  что случилось
+  // state -текущее состояние redux
+  // action что случилось
+
   switch (action.type) {
-// action.type ОБЯЗАТЕЛЬНО
+// .type ОБЯЗАТЕЛЬНО 
+
     case "INC": {
       // хотелось бы просто увеличить state.cnt
       // но редьюсер ВСЕГДА должен возвращаеть новый state а не изменять старый!
       console.log('state до обработки редьюсером:',state);
-      let newState={...state}; //делаем поверхностную копию старого state ВСЕГДА ИММУТАБЕЛЬНО
+      let newState={...state}; //делаем поверхностную копию старого state
       newState.cnt++;
       console.log('state после обработки редьюсером:',newState);
       return newState;
