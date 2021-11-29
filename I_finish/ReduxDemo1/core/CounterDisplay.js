3e19e19431482c0fbac6a2c60ae204d7dc30bac0
AlexandraHessen <xl_energy@mail.ru>
2021-11-29 21:41:53 +0300

    CounterDisplay

--- I_finish/ReduxDemo1/core/CounterDisplay.js
+++ I_finish/ReduxDemo1/core/CounterDisplay.js
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+// позволяет React компонентам подписаться на Redux 
 
 class intCounterDisplay extends React.PureComponent {
 
@@ -21,9 +22,12 @@ class intCounterDisplay extends React.PureComponent {
 }
 
 const mapStateToProps = function (state) {
+  // этот хэш говорит что из Redax под какими props должно прилететь мне в компонент
   return {
     // из раздела Redux с именем counter свойство cnt будет доступно
     // данному компоненту как this.props.cnt
+
+    // я хочу в качестве props получить cnt 
     cnt: state.counter.cnt,
   };
 };
@@ -31,7 +35,7 @@ const mapStateToProps = function (state) {
 // присоединяем (connect) компонент к хранилищу Redux
 // заворачиваем в HOC
 // connect  import {connect} from 'react-redux'; 
-// позволяет React подписаться на Redux 
+// позволяет React компонентам подписаться на Redux 
 // connect в качестве аргумента дожен получить хэш, который говорит 
 // что из Redux под какими props должно прилететь
 const CounterDisplay = connect(mapStateToProps)(intCounterDisplay);
