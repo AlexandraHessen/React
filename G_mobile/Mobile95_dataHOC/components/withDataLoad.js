e73a69eb3d24094bd5c5b339efe1c46fe5b46a41
AlexandraHessen <xl_energy@mail.ru>
2021-12-01 18:41:22 +0300

    HOC withDataLoad

--- G_mobile/Mobile95_dataHOC/components/withDataLoad.js
+++ G_mobile/Mobile95_dataHOC/components/withDataLoad.js
@@ -2,29 +2,43 @@ import React from 'react';
 import isoFetch from 'isomorphic-fetch';
 
 let withDataLoad = (fetchConfig,propName) => Component => {
+  // fetchConfig - конфиг для обращения URL: "http://fe.it-academy.by/TestFetch.php",  method: 'post'
+  // propName - под каким именем будет передан props компоненту
 
     class ComponentWithDataLoad extends React.PureComponent {
 
+// ---------------------- 1 ---------------------- //
+/* в componentDidMount делаем запрос на загрузку данных */
         componentDidMount() {
           this.loadData();
         }
       
         state = {
           dataReady: false, // готовы ли данные
-          combinedProps: null, // исходные пропсы, переданные HOC-у, плюс пропс propName с загруженными данными
+          combinedProps: null, // данные которые мы загружаем + propsы которые уже были у оборачиваемого компонента
         };
       
+// ---------------------- 3 ---------------------- //
+/* ошибка при загрузке данных */
         fetchError = (errorMessage) => {
           console.error(showStr);
         };
       
+// ---------------------- 3 ---------------------- //
+/* полученные данные кладем в state */
         fetchSuccess = (loadedData) => {
           this.setState({
-            dataReady:true,
+            dataReady:true, //делаем для того чтобы пока данные не нагружены компонент не рендариться 
             combinedProps:{...this.props,[propName]:loadedData},
+            // ложим в  combinedProps все propsы которые уже были у оборачиваемого компонента + данные которые мы загрузили 
+            // ...this.props - propsы которые уже были у оборачиваемого компонента     делаем для того чтобы наш оборачиваемый компонент помимо прорса с нашими данными мог получить и другие props
+            // propName - под каким именем будет передан props с данными которые загрузили  компоненту
+            // loadedData - сами данные которые загрузили 
           });
         };
       
+// ---------------------- 2 ---------------------- //
+/* функция загрузки данных */        
         loadData = () => {
       
           isoFetch(fetchConfig.URL, fetchConfig)
@@ -51,6 +65,7 @@ let withDataLoad = (fetchConfig,propName) => Component => {
             return <div>загрузка данных...</div>;
       
           return <Component {...this.state.combinedProps} /> ;
+          // ...this.state.combinedProps - передаются сразу все propsы. и те которые были + наши полученные данные
         }
       
       }
