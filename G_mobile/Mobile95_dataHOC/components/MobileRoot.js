da316902b0e08ab6f967fb4b7e1e7a3f9c9d6f85
AlexandraHessen <xl_energy@mail.ru>
2021-12-01 18:40:25 +0300

    MobileRoot

--- G_mobile/Mobile95_dataHOC/components/MobileRoot.js
+++ G_mobile/Mobile95_dataHOC/components/MobileRoot.js
@@ -1,10 +1,13 @@
 ﻿import React from 'react';
 
 import MobileCompany from './MobileCompany';
+// MobileCompany - компонент которому нужны данные
 import { withDataLoad } from './withDataLoad';
+// withDataLoad - HOC по загрузке данных
 
 class MobileRoot extends React.PureComponent {
 
+// ---------------------- 2 ---------------------- /
   fetchConfig={
     URL: "http://fe.it-academy.by/TestFetch.php",
     method: 'post',
@@ -13,9 +16,15 @@ class MobileRoot extends React.PureComponent {
     },
   };
 
+
+// ---------------------- 1 ---------------------- /
   // HOC возвращает каждый раз НОВЫЙ, обёрнутый компонент
   // поэтому оборачивать в HOC лучше не внутри render, чтобы не рендерить каждый раз НОВЫЙ компонент
   MobileCompanyWithData=withDataLoad(this.fetchConfig,"companyData")(MobileCompany);
+  // this.fetchConfig - загрузить данные по этому конфигу
+  // "companyData" - передай компоненту MobileCompany под этим именем 
+  // MobileCompany - компонент которому нужны данные
+  // withDataLoad - HOC по загрузке данных
 
   render() {
 
