117d56f7f5d2375999291ee5e93c618c261714d1
AlexandraHessen <xl_energy@mail.ru>
2021-12-03 22:24:51 +0300

    MainPage

--- I_finish/Redux_AJAX1/core/MainPage.js
+++ I_finish/Redux_AJAX1/core/MainPage.js
@@ -1,11 +1,13 @@
 ﻿import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+// createStore - создаем Redax
 
 import combinedReducer from '../redux/reducers.js';
 import CountriesList from '../core/CountriesList';
 
 let store=createStore(combinedReducer);
+// Redux создай store которым управляет этот reducer - combinedReducer(который в себе объединяет все reduserы)
 
 class MainPage extends React.PureComponent {
 
@@ -13,6 +15,10 @@ class MainPage extends React.PureComponent {
 
     return (
       <Provider store={store}>
+      {/* Provider - import { Provider } from 'react-redux'; 
+      всё свое приложения мы заварачиваем в tag Provider
+      тег Provider говорит, что для всего кода, который внутри будет работать Redux с хранилищем store
+      */}
           <div>
               <h1>Страны</h1>
               <CountriesList />
