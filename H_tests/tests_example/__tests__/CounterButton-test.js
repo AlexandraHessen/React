c9769ffe52d04c9d8da7e2b9ad857fd9d878da15
AlexandraHessen <xl_energy@mail.ru>
2021-12-03 22:28:02 +0300

    CounterButton-test

--- H_tests/tests_example/__tests__/CounterButton-test.js
+++ H_tests/tests_example/__tests__/CounterButton-test.js
@@ -4,20 +4,21 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import CounterButton from '../components/CounterButton';
+//импортируем файл который будем тестировать
 
 test('работа CounterButton', () => {
 
-  // создаём тестовую версию компонента
+  // создаём тестовую версию компонента который будем тестировать
   const component = renderer.create(
     <CounterButton />
   );
 
-  // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+  // получаем снэпшот (HTML-снимок) компонента для сверки, чтобы вёрстка не испортилась
   let componentTree=component.toJSON();
   expect(componentTree).toMatchSnapshot();
 
   // найдём в вёрстке компонента саму кнопку
-  const buttonElem = component.root.find( el => el.type=='input' /*&& el.props.aaa == 'bbb'*/ ); 
+  const buttonElem = component.root.find( el => el.type=='input' /*&& el.props.aaa == 'bbb'*/ );  //или по пропсам найти
   // и "нажмём" на неё
   buttonElem.props.onClick();
 
