229f586ff071de8dc15984e5b2b6ba98d40a9f81
AlexandraHessen <xl_energy@mail.ru>
2021-12-05 23:34:26 +0300

    VotesBlock510_hoc_carry

---
+++ F_VB5/VotesBlock510_hoc_carry/components/withColorBackground.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+
+function withColorBackground(color) {
+    return function(Component) {
+      return props => (
+        <div style={{backgroundColor:color}}>
+          <Component {...props} />
+        </div>
+      );
+    };
+}
+
+
+//  Тоже самое в стрелочном стиле
+/*
+let withColorBackground = color => Component => props =>{
+// let withColorBackground = (color, width) => Component => props =>
+    <div style={{backgroundColor:color}}>
+      <Component {...props} />
+    </div>
+;
+}
+
+*/
+
+export { withColorBackground };
