77dc47052b040c38049b507f415f058452a30e1e
AlexandraHessen <xl_energy@mail.ru>
2021-12-05 23:33:24 +0300

    VotesBlock500_hoc

---
+++ F_VB5/VotesBlock500_hoc/components/withColorBackground.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+function withColorBackground(Component) {
+    //  начинать название с with
+
+    // HOC withColorBackground должен вернуть КОМПОНЕНТ
+    // в данном случае возвращаем компонент в функциональном стиле
+    return props => (
+      <div style={{backgroundColor:"yellow"}}>
+        <Component {...props} /> 
+        {/* {...props} передаем все пропсы компонента, пробрасываем их */}
+      </div>
+    );
+}
+
+export { withColorBackground };
