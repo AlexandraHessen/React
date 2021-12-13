73656b7bcfbbcff3339782058ed45ec493348fbc
AlexandraHessen <xl_energy@mail.ru>
2021-12-13 23:56:54 +0300

    CountriesList

--- I_finish/Redux_AJAX1/core/CountriesList.js
+++ I_finish/Redux_AJAX1/core/CountriesList.js
@@ -14,7 +14,11 @@ class CountriesList extends React.PureComponent {
   componentDidMount() {
 
     this.props.dispatch( countriesLoadingAC() ); // переводим раздел countries стора в состояние "загружается"
+    // значит this.props.dispatch( { type:"COUNTER_BUTTON_ADD" } );
+    // мы вызываем функцию из файла с экшенами (countriesAC) которая возвращает такой хэш
+    //  {    type:"COUNTER_BUTTON_ADD"}
 
+    // отправляем AJAX запрос
     isoFetch("http://fe.it-academy.by/Examples/net_city/countries.json")
     // isoFetch - работает с промисами
     // запросить json по ссылке
@@ -28,8 +32,14 @@ class CountriesList extends React.PureComponent {
             else
                 return response.json();
         })
-        .then( (data) => {
-            this.props.dispatch( countriesSetAC(data.rows) ); // переводим раздел countries стора в состояние "ошибка"
+        .then( (data) => { //когда данные хорошо загружены
+            this.props.dispatch( countriesSetAC(data.rows) );
+            // значит this.props.dispatch( { type: COUNTRIES_SET } );
+            // мы вызываем функцию из файла с экшенами (countriesAC) которая возвращает такой хэш
+            // {
+            //   type: COUNTRIES_SET,
+            //   countries:countries, //передаем загруженные данные
+            // };
         })
         .catch( (error) => {
             console.error(error);
@@ -47,12 +57,15 @@ class CountriesList extends React.PureComponent {
     if ( this.props.countries.status===2 )
       return "ошибка загрузки данных";
 
+
+    // if ( this.props.countries.status===3 )
+    // все хорошо данные загружены
     return (
       <ul>
         {
           this.props.countries.data.map( (countryInfo,index) => <li key={index}>{countryInfo[1]}</li> )
-          // this.props.countries / весь раздел Redux 
-          // this.props.countries.data - список стран
+          // this.props.countries / весь раздел Redux даступный нам на который мы подписались через connect(mapStateToProps)
+          // this.props.countries.data - список стран 
         }
       </ul>
     );
@@ -63,8 +76,14 @@ class CountriesList extends React.PureComponent {
 
 const mapStateToProps = function (state) {
   return {
+    // весь раздел Redux state под именем counters будет доступен
+    // данному компоненту как this.props.counters
+    // это значит что в this.props.countries нам доспун хэш из countriesReducer
+    // { status: 0, // 0 - ничего не началось, 1 - идёт загрузка, 2 - была ошибка, 3 - данные загружены
+    //    data: null, }
     countries: state.countries,
   };
 };
 
 export default connect(mapStateToProps)(CountriesList);
+// connect(mapStateToProps) - значит подписан на redux 
